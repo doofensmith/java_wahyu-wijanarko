d9515231347012c9c0db5ff4b59b1a0fbf76a86b
doofensmith <wahyuwijanarko3998@gmail.com>
2022-02-24 17:03:41 +0700

    on progress

---
+++ 4_Basic Programming/praktikum/Task1VokalKonsonan.java
@@ -0,0 +1,46 @@
+public class Task1VokalKonsonan {
+    public static void main(String[] args) {
+        
+        //Test String :
+        String testString = "Alterra Indonesia";
+
+        //[PROSES]:
+        process(testString);
+
+    }
+
+    //Method for process
+    private static void process(String inputString) {
+        //[PROSES]:
+        //array huruf vokal dan konsonan
+        char[] hurufVokal = {'a','i','u','e','o'};
+        char[] hurufKonsonan = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
+        
+        //Perulangan untuk mencari jumlah huruf vokal dan konsonan
+        int jumlahHurufVokal = 0;
+        int jumlahHurufKonsonan = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            //Jumlah Huruf Konsonan
+            for (int j = 0; j < hurufKonsonan.length; j++) {
+                if (inputString.toLowerCase().charAt(i) == hurufKonsonan[j]) {
+                    jumlahHurufKonsonan++;
+                }
+            }
+            
+            //Jumlah Huruf Vokal
+            for (int j = 0; j < hurufVokal.length; j++) {
+                if (inputString.toLowerCase().charAt(i) == hurufVokal[j]) {
+                    jumlahHurufVokal++;
+                }
+            }
+            
+        }
+        
+        //Output
+        System.out.println("Jumlah Huruf Vokal = "+jumlahHurufVokal);
+        System.out.println("Jumlah Huruf Konsonan = "+jumlahHurufKonsonan);
+        System.out.println("Total Karakter = "+inputString.length());
+        
+    }
+
+}
