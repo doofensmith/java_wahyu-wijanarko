c2d388632b64cbd9c7e4c6229134854dfbcaac57
doofensmith <wahyuwijanarko3998@gmail.com>
2022-03-04 19:32:54 +0700

    problem3 done

--- 9_Problem Solving Brute Force/praktikum/Problem1.java
+++ 9_Problem Solving Brute Force/praktikum/Problem1.java
@@ -1,5 +1,19 @@
 public class Problem1 {
     public static void main(String[] args) {
+        //input 
+        int A = 6;
+        int B = 6;
+        int C = 14;
+
+
+    }
+
+    //simple equation
+    private static void simpleEquation(int A, int B, int C) {
+        int x = 0;
+        int y = 0;
+        int z = 0;
+
         
     }
 }
