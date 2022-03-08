655b976080410ab9edfca82cfdd60e4a8b7ebd25
doofensmith <wahyuwijanarko3998@gmail.com>
2022-03-08 15:32:10 +0700

    section 10 prob 3 rev..

--- 10_Problem Solving Dynamic Programming/praktikum/Problem3.java
+++ 10_Problem Solving Dynamic Programming/praktikum/Problem3.java
@@ -5,10 +5,14 @@ public class Problem3 {
 
         //input
         int[] stone = {10, 30, 40, 20};
+        int[] stone2 = {30, 10, 60, 10, 60, 50};
 
         //output
         System.out.println("Input : "+ Arrays.toString(stone));
-        System.out.println("Output : "+frog(stone));
+        System.out.println("Output : "+frog2(stone));
+
+        System.out.println("\nInput : "+ Arrays.toString(stone2));
+        System.out.println("Output : "+frog2(stone2));
 
     }
 
@@ -32,5 +36,27 @@ public class Problem3 {
         }
 
         return frogJump;
+    }
+
+    //lompatan katak maju atau mundur batu
+    private static int frog2(int[] arr) {
+
+        int prevStone1 = 0;
+        int prevStone2 = 0;
+        for (int i = 1; i < arr.length; i++) {
+
+            int doubleJump = Integer.MAX_VALUE;
+            int singleJump = prevStone1 + Math.abs(arr[i] - arr[i-1]);
+            if (i>1) {
+                doubleJump = prevStone2 + Math.abs(arr[i] - arr[i-2]);
+            }
+            int currentStone = Math.min(singleJump,doubleJump);
+            prevStone2 = prevStone1;
+            prevStone1 = currentStone;
+
+        }
+
+
+        return prevStone1;
     }
 }
