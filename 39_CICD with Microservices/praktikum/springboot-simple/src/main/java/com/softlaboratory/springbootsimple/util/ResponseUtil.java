c49d190e982445e0fac99b5a410ee56fc8051cc8
doofensmith <wahyuwijanarko3998@gmail.com>
2022-04-13 16:47:54 +0700

    sect39 ci/cd pipeline

---
+++ 39_CICD with Microservices/praktikum/springboot-simple/src/main/java/com/softlaboratory/springbootsimple/util/ResponseUtil.java
@@ -0,0 +1,33 @@
+package com.softlaboratory.springbootsimple.util;
+
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@SuperBuilder
+public class ResponseUtil extends BaseResponse {
+
+    private ResponseUtil() {
+        //
+    }
+
+    public static ResponseEntity<Object> build(HttpStatus status, String message, Object data) {
+
+        BaseResponse response = BaseResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .responseCode(String.valueOf(status.value()))
+                .message(message)
+                .data(data)
+                .build();
+
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("code", code);
+//        map.put("data", data);
+
+        return new ResponseEntity<>(response, status);
+
+    }
+
+}
